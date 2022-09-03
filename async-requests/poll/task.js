c3bbfe0c4c5e7ab2fed536fe7aa9a6b09972b850
Сергей Сапожников <amegasv@gmail.com>
2022-09-03 17:24:31 +0500

    Homework completed

---
+++ async-requests/poll/task.js
@@ -0,0 +1,23 @@
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
+xhr.responseType = 'json'
+xhr.send();
+
+xhr.onload = () => {
+    let responseArr = xhr.response;
+    console.log(responseArr)
+
+	let Title = document.getElementById('poll__title');
+	Title.textContent = responseArr.data.title;
+
+	let answers = document.getElementById('poll__answers');
+	responseArr.data.answers.forEach(answer => {
+		answers.insertAdjacentHTML('beforeEnd', 
+        `<button class="poll__answer">${answer}</button>`)
+	});
+
+	let btnAnswer = answers.querySelectorAll('.poll__answer');
+    btnAnswer.forEach(btn => btn.onclick = () => {
+		alert("Спасибо, ваш голос засчитан!");
+	});
+}
---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,16 @@
+const progress = document.getElementById('progress');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData(form);
+
+    xhr.upload.addEventListener('progress', function (event) {
+        progress.value = event.loaded / event.total;
+    });
+    
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+    xhr.send(formData);
+})
