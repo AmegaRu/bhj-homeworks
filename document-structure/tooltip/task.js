bab3ef461dbf99214e079cd8b6df550678fa08be
Сергей Сапожников <amegasv@gmail.com>
2022-09-05 22:37:05 +0500

    Fixed_2

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,12 +5,13 @@ for (let i = 0; i < hasTooltip.length; i++) {
   const tooltip = hasTooltip[i];
 
   tooltip.addEventListener('click', function(evt) {
+
     tool.classList.add('tooltip');
     tool.classList.toggle('tooltip_active');
-    tool.setAttribute('style', `left: ${tooltip.getBoundingClientRect().left}px; top: ${tooltip.getBoundingClientRect().bottom}px; position:absolute`);
-    tool.innerText = `${tooltip.title}`;
+    tool.setAttribute('style', `left: ${tooltip.getBoundingClientRect().left}px; top: ${tooltip.getBoundingClientRect().bottom}px;`);
+    tool.innerText = tooltip.title;
     tooltip.insertAdjacentElement('beforeBegin', tool);
-    
+        
     evt.preventDefault();
   })
 }
